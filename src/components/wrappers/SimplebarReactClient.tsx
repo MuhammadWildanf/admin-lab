9f2224fb5dd9cb342a0751a4afd3a0644e0f2ad3
Muhammad Wildan Ferdiansyah <92373802+MuhammadWildanf@users.noreply.github.com>
2025-05-21 16:42:06 +0700

    fix simplebar

--- src/components/wrappers/SimplebarReactClient.tsx
+++ src/components/wrappers/SimplebarReactClient.tsx
@@ -1,8 +1,20 @@
-import type { ChildrenType } from '@/types/component-props'
-import type SimpleBarCore from 'simplebar-core'
-import SimpleBar, { type Props } from 'simplebar-react'
+// import type { ChildrenType } from '@/types/component-props'
+// import type SimpleBarCore from 'simplebar-core'
+// import SimpleBar, { type Props } from 'simplebar-react'
 
-type SimplebarReactClientProps = React.ForwardRefExoticComponent<Props & React.RefAttributes<SimpleBarCore | null>>['defaultProps'] & ChildrenType
+// type SimplebarReactClientProps = React.ForwardRefExoticComponent<Props & React.RefAttributes<SimpleBarCore | null>>['defaultProps'] & ChildrenType
+
+// const SimplebarReactClient = ({ children, ...options }: SimplebarReactClientProps) => {
+//   return <SimpleBar {...options}>{children}</SimpleBar>
+// }
+
+// export default SimplebarReactClient
+
+import type { HTMLAttributes } from 'react'
+import SimpleBar from 'simplebar-react'
+import type { ChildrenType } from '@/types/component-props'
+
+type SimplebarReactClientProps = HTMLAttributes<HTMLDivElement> & ChildrenType
 
 const SimplebarReactClient = ({ children, ...options }: SimplebarReactClientProps) => {
   return <SimpleBar {...options}>{children}</SimpleBar>
--- src/types/simplebar-react.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'simplebar-react' {
-  import * as React from 'react'
-  export default class SimpleBar extends React.Component<any, any> {}
-}
